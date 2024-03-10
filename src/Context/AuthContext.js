82c9c94ffc5a3557beb84bdb7cd59508bfa0b1e6
Love-Becca <follycube2020@gmail.com>
2024-03-10 15:56:21 +0100

    remove unused

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,5 +1,4 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { FOODIMETRIC_HOST_URL } from '../utils/getData';
 
 const AuthContext = createContext();
 
--- src/Pages/FindAlternative.js
+++ src/Pages/FindAlternative.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const FindAlternative = () => {
     return (
