7ac05995c643a3460879790d053d7613fc7b4b5e
Love-Becca <follycube2020@gmail.com>
2025-03-07 15:55:03 +0100

    fix z-index

--- src/Components/Nav/SearchNav.jsx
+++ src/Components/Nav/SearchNav.jsx
@@ -27,7 +27,7 @@ const SearchNav = () => {
     }
     return (
         <>
-            <div className="fixed top-0 left-0 bg-white h-16 z-10 w-60 md:hidden flex items-center border-b border-gray-300">
+            <div className="fixed top-0 left-0 bg-white h-16 z-30 w-60 md:hidden flex items-center border-b border-gray-300">
                 <button onClick={toggleMenu} className="text-gray-700 hover:text-gray-900 focus:outline-none p-2">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
